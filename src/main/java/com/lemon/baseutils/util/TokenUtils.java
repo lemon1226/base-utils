e8cce2c29cb8e7a7d6933cf81feffcec3f39d8b2
zhangmeng <32800438@qq.com>
2019-07-25 10:44:53 +0800

    修复bug

--- src/main/java/com/lemon/baseutils/util/TokenUtils.java
+++ src/main/java/com/lemon/baseutils/util/TokenUtils.java
@@ -39,7 +39,7 @@ public class TokenUtils {
      */
     public static Boolean validateToken(String token, String usernameFlag, String passwordFlag, String secret, Date lastPasswordReset) {
         final String username = getUsernameFromToken(token, secret);
-        final String password = getUsernameFromToken(token, secret);
+        final String password = getPasswordFromToken(token, secret);
         final Date created = getCreatedDateFromToken(token, secret);
 
         return (username.equals(usernameFlag) &&
